a5c13b116ee7c0ec1e072e4e11c59501558d0b5b
Azevedo <pedrohxpa@hotmail.com>
2023-06-06 14:17:00 -0400

    Primeiro commit

---
+++ testeTryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Write("Informe o primeiro valor: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Informe o segundo número: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int r = a / b;
+    Console.WriteLine($"O resultado é {r}");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Não é permitido divisão por zero! {ex.Message}");
+}
+catch (InvalidCastException ex)
+{
+    Console.WriteLine($"Conversão de string para letra não é permitida! {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Aqui passa de qualquer forma!");
+}
